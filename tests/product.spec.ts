60d5cb2b3e11da68d543965d342ff17b97a47165
Kang Hyeon Jun <rkd980609@naver.com>
2025-08-11 22:13:43 +0900

    상세 상품

--- tests/product.spec.ts
+++ tests/product.spec.ts
@@ -1,10 +1,17 @@
 import { test, expect } from '@playwright/test';
 
 test('상품 상세 페이지 진입', async ({ page }) => {
-  await page.goto('/');
-  await page.fill('input[name="searchQuery"]', '셔츠');
-  await page.keyboard.press('Enter');
 
-  await page.click('.list-box .img-block >> nth=0');
-  await expect(page).toHaveURL(/goods/);
+   //상품 상세 페이지로 이동
+  await page.goto('https://www.musinsa.com/products/3976350');
+
+  // 옵션 선택 버튼 클릭
+  await page.waitForSelector('[data-button-id="option_type"]');
+  await page.click('[data-button-id="option_type"]');
+
+  // 사이즈 선택
+  await page.click('text=100');
+
+  // 장바구니 담기
+  await page.click('button:has-text("장바구니")');
 });
