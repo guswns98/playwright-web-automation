f015a0d63482be2aa5604c69d6b30c1b0858d999
Kang Hyeon Jun <rkd980609@naver.com>
2025-08-11 22:07:41 +0900

    옵션 선택 후 장바구니 이동

--- tests/scenario.spec.ts
+++ tests/scenario.spec.ts
@@ -20,14 +20,19 @@ test('로그인 후 상품 장바구니 담기 및 결제 흐름', async ({ page
   //상품 상세 페이지로 이동
   await page.goto('https://www.musinsa.com/products/3976350');
 
-//   //사이즈 선택 (필요한 경우)
-//   await page.selectOption('select[name="size"]', '100');
+  // 옵션 선택 버튼 클릭
+  await page.waitForSelector('[data-button-id="option_type"]');
+  await page.click('[data-button-id="option_type"]');
 
-//   //장바구니 담기
-//   await page.click('button.btn-cart');
+  // 사이즈 선택
+  await page.click('text=100');
 
-//   //장바구니 페이지로 이동
-//   await page.goto('https://www.musinsa.com/cart');
+  // 장바구니 담기
+  await page.click('button:has-text("장바구니")');
+
+
+  //장바구니 페이지로 이동
+  await page.goto('https://www.musinsa.com/orders/cart');
 
 //   //장바구니에 상품이 담겼는지 확인
 //   await expect(page.locator('.cart')).toContainText('상품명'); 
