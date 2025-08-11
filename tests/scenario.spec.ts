cfc765579bb15fcc73e0ab7017c9158c0b8b9e03
Kang Hyeon Jun <rkd980609@naver.com>
2025-08-12 01:21:04 +0900

    드롭다운 체크

--- tests/scenario.spec.ts
+++ tests/scenario.spec.ts
@@ -3,61 +3,83 @@ import { MUSINSA_ID, MUSINSA_PW } from '../utils/env';
 import { randomDelay } from '../utils/login';
 
 test('로그인 후 상품 장바구니 담기 및 결제 흐름', async ({ page, request }) => {
-  //로그인 페이지로 이동
-  await page.goto('https://www.musinsa.com/auth/login');
+    //로그인 페이지로 이동
+    await page.goto('https://www.musinsa.com/auth/login');
 
-  //로그인 정보 입력
-  await page.waitForSelector('input[title="아이디 입력"]');
-  await page.fill('input[title="아이디 입력"]', MUSINSA_ID,);
-  await page.waitForTimeout(randomDelay());
-  await page.fill('input[title="비밀번호 입력"]', MUSINSA_PW);
-  await page.waitForTimeout(randomDelay());
-  await page.click('button[type="submit"]');
+    //로그인 정보 입력
+    await page.waitForSelector('input[title="아이디 입력"]');
+    await page.fill('input[title="아이디 입력"]', MUSINSA_ID,);
+    await page.waitForTimeout(randomDelay());
+    await page.fill('input[title="비밀번호 입력"]', MUSINSA_PW);
+    await page.waitForTimeout(randomDelay());
+    await page.click('button[type="submit"]');
 
- // CAPTCHA 감지 시 
-  const captcha = page.locator('iframe[src*="captcha"]');
-  if (await captcha.isVisible()) {
-    console.log('CAPTCHA가 감지 수동 클릭');
-  await page.pause();
-}
+    // CAPTCHA 감지 시 
+    const captcha = page.locator('iframe[src*="captcha"]');
+    if (await captcha.isVisible()) {
+        console.log('CAPTCHA가 감지 수동 클릭');
+    await page.pause();
+    }
 
-  //로그인 성공 확인
-  await expect(page).toHaveURL(/recommend/);
+    //로그인 성공 확인
+    await expect(page).toHaveURL(/recommend/);
 
 
-  //상품 상세 페이지로 이동
-  await page.goto('https://www.musinsa.com/products/3976350');
+    //상품 상세 페이지로 이동
+    await page.goto('https://www.musinsa.com/products/3976290');
 
-  // 옵션 선택 버튼 클릭
-  await page.waitForSelector('[data-button-id="option_type"]');
-  await page.click('[data-button-id="option_type"]');
+    // 드롭다운 선택 버튼 클릭
+    await page.waitForSelector('[data-button-id="option_type"]');
+    await page.click('[data-button-id="option_type"]');
 
-  // 사이즈 선택
-  await page.click('text=100');
+    // 항목 선택 후 클릭 
+    await page.waitForSelector('[data-button-id="select_optionvalue"] >> text=240');
+    await page.click('[data-button-id="select_optionvalue"] >> text=240');
 
-  // 장바구니 담기
-  await page.click('button:has-text("장바구니")');
+
+    // 장바구니 담기
+    const basketButton = document.querySelector('button[data-button-id="basket_btn"]') as HTMLButtonElement;
+
+    if (basketButton) {
+    basketButton.addEventListener('click', () => {
+    console.log('장바구니 버튼 클릭됨');
+    });
+    }
 
 
   //장바구니 페이지로 이동
   await page.goto('https://www.musinsa.com/orders/cart');
 
 
-  //API로 장바구니 데이터 검증
-  const cookies = await page.context().cookies();
-  const cookieHeader = cookies.map(c => `${c.name}=${c.value}`).join('; ');
+//   //API 장바구니 데이터 검증
+//   const cookies = await page.context().cookies();
+//   const cookieHeader = cookies.map(c => `${c.name}=${c.value}`).join('; ');
 
-  const cartRes = await request.get('https://www.musinsa.com/api/orders/cart', {
-    headers: {
-      cookie: cookieHeader,
-      accept: 'application/json'
-    }
-  });
+//   const cartRes = await request.get('https://like.musinsa.com/like/api/v1/members/liketypes/goods?page=0&size=100', {
+//     headers: {
+//       cookie: cookieHeader,
+//       accept: 'application/json'
+//     }
+//   });
 
-  expect(cartRes.status()).toBe(200);
-  const cartData = await cartRes.json();
-  console.log('장바구니 API 응답:', cartData);
-  expect(cartData.items.some((item: any) => item.product_no === '3976350')).toBeTruthy();
+//   expect(cartRes.status()).toBe(200);
+//   const cartData = await cartRes.json();
+//   console.log('장바구니 API 응답:', cartData);
+
+//   // 상품 번호를 문자열로 변환해서 비교
+//   const isProductInCart = cartData.data.contents.content.some(
+//   (item: any) => String(item.productNo) === '3976350'
+//   );
+
+//   // 디버깅용 로그
+//   if (!isProductInCart) {
+//   console.error('상품이 장바구니에 없습니다. 현재 장바구니 목록:', 
+//     cartData.data.contents.content.map((item: any) => item.productNo)
+//    );
+//  }
+
+//   expect(isProductInCart).toBe(true);
+
 
   // 팝업 닫기 버튼 클릭 
   await page.click('button:has-text("닫기")');
