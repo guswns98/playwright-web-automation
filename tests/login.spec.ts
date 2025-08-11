1fd6ba0e5c7f101d705e5bee9c61f7b54d81a17f
Kang Hyeon Jun <rkd980609@naver.com>
2025-08-11 18:33:59 +0900

    login

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,10 +1,13 @@
 import { test, expect } from '@playwright/test';
+import { MUSINSA_ID, MUSINSA_PW } from '../utils/env';
 
 test('회원 로그인', async ({ page }) => {
-  await page.goto('https://www.musinsa.com');
-  await page.click('text=로그인'); // 버튼 텍스트 기반 클릭
-  await page.fill('#id', 'your_id');
-  await page.fill('#pw', 'your_password');
-  await page.click('button[type=submit]');
-  await expect(page).toHaveURL(/musinsa\.com/);
+  //로그인 페이지로 이동
+  await page.goto('https://www.musinsa.com/auth/login');
+
+  //로그인 정보 입력
+  await page.waitForSelector('input[title="아이디 입력"]');
+  await page.fill('input[title="아이디 입력"]', MUSINSA_ID);
+  await page.fill('input[title="비밀번호 입력"]', MUSINSA_PW);
+  await page.click('button[type="submit"]');
 });
