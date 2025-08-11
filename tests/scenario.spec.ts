99a45fdd99240dcf1d341437f0a5cad6edebffd5
Kang Hyeon Jun <rkd980609@naver.com>
2025-08-11 23:43:10 +0900

    request api

--- tests/scenario.spec.ts
+++ tests/scenario.spec.ts
@@ -1,21 +1,29 @@
 import { test, expect } from '@playwright/test';
 import { MUSINSA_ID, MUSINSA_PW } from '../utils/env';
+import { randomDelay } from '../utils/login';
 
-
-test('로그인 후 상품 장바구니 담기 및 결제 흐름', async ({ page }) => {
+test('로그인 후 상품 장바구니 담기 및 결제 흐름', async ({ page, request }) => {
   //로그인 페이지로 이동
   await page.goto('https://www.musinsa.com/auth/login');
 
   //로그인 정보 입력
   await page.waitForSelector('input[title="아이디 입력"]');
-  await page.fill('input[title="아이디 입력"]', MUSINSA_ID);
+  await page.fill('input[title="아이디 입력"]', MUSINSA_ID,);
+  await page.waitForTimeout(randomDelay());
   await page.fill('input[title="비밀번호 입력"]', MUSINSA_PW);
+  await page.waitForTimeout(randomDelay());
   await page.click('button[type="submit"]');
 
+ // CAPTCHA 감지 시 
+  const captcha = page.locator('iframe[src*="captcha"]');
+  if (await captcha.isVisible()) {
+    console.log('CAPTCHA가 감지 수동 클릭');
+  await page.pause();
+}
 
   //로그인 성공 확인
   await expect(page).toHaveURL(/recommend/);
-  //await expect(page.locator('aria-label="로그아웃"')).toBeVisible();
+
 
   //상품 상세 페이지로 이동
   await page.goto('https://www.musinsa.com/products/3976350');
@@ -34,11 +42,33 @@ test('로그인 후 상품 장바구니 담기 및 결제 흐름', async ({ page
   //장바구니 페이지로 이동
   await page.goto('https://www.musinsa.com/orders/cart');
 
-//   //장바구니에 상품이 담겼는지 확인
-//   await expect(page.locator('.cart')).toContainText('상품명'); 
 
-//   //결제 버튼 클릭
-//   await page.click('text=구매하기');
+  //API로 장바구니 데이터 검증
+  const cookies = await page.context().cookies();
+  const cookieHeader = cookies.map(c => `${c.name}=${c.value}`).join('; ');
+
+  const cartRes = await request.get('https://www.musinsa.com/api/orders/cart', {
+    headers: {
+      cookie: cookieHeader,
+      accept: 'application/json'
+    }
+  });
+
+  expect(cartRes.status()).toBe(200);
+  const cartData = await cartRes.json();
+  console.log('장바구니 API 응답:', cartData);
+  expect(cartData.items.some((item: any) => item.product_no === '3976350')).toBeTruthy();
+
+  // 팝업 닫기 버튼 클릭 
+  await page.click('button:has-text("닫기")');
+
+  //장바구니에 상품이 담겼는지 확인
+  const cartItem = page.locator('div.cart-goods[data-cart-id="1320505753"]');
+  await expect(cartItem).toBeVisible();
+
+
+  //결제 버튼 클릭
+  await page.click('button.cart-float__button.is-active');
 
 //   //결제 페이지로 이동 확인
 //   await expect(page).toHaveURL(/order/);
