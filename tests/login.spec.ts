ffeee469d76da905d224747e0d2784649d1ebfb8
Kang Hyeon Jun <rkd980609@naver.com>
2025-08-11 22:55:09 +0900

    captcha 감지

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -10,6 +10,13 @@ test('회원 로그인', async ({ page }) => {
   await page.fill('input[title="아이디 입력"]', MUSINSA_ID);
   await page.fill('input[title="비밀번호 입력"]', MUSINSA_PW);
   await page.click('button[type="submit"]');
+  
+   // CAPTCHA 감지
+  const captcha = page.locator('iframe[src*="captcha"]');
+  if (await captcha.isVisible()) {
+    console.log('CAPTCHA가 감지 수동 클릭');
+  await page.pause(); 
+}
 
   //로그인 성공 확인
   await expect(page).toHaveURL(/recommend/);
