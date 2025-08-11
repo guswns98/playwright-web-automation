f619562c80495070d2e24d8ea63b55475d3f8994
Kang Hyeon Jun <rkd980609@naver.com>
2025-08-11 22:11:31 +0900

    로그인 확인

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -10,4 +10,10 @@ test('회원 로그인', async ({ page }) => {
   await page.fill('input[title="아이디 입력"]', MUSINSA_ID);
   await page.fill('input[title="비밀번호 입력"]', MUSINSA_PW);
   await page.click('button[type="submit"]');
+
+  //로그인 성공 확인
+  await expect(page).toHaveURL(/recommend/);
+
 });
+
+
