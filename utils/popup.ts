d4f01385088b0f56dd0eb08c229449381663ddb3
Kang Hyeon Jun <rkd980609@naver.com>
2025-08-12 09:58:07 +0900

    move

--- utils/popup.ts
+++
@@ -1,7 +0,0 @@
-export async function closePopupIfVisible(page: any) {
-  const closeBtn = page.locator('.bottom-sheet-musinsa-money__button');
-  if (await closeBtn.isVisible()) {
-    await closeBtn.click();
-    await page.waitForSelector('.bottom-sheet-musinsa-money__button', { state: 'hidden' });
-  }
-}
