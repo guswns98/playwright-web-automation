f46b9b6e4dd296c267390138b9d5016794d34134
Kang Hyeon Jun <rkd980609@naver.com>
2025-08-13 15:43:35 +0900

    captcha 수정

--- tests/utils/captcha.ts
+++ tests/utils/captcha.ts
@@ -1,9 +1,13 @@
 import { Page } from "playwright-core";
 
 export async function CAPTCHAIfVisible(page: Page): Promise<void> {
-  const captchaFrame = page.frameLocator('iframe[src*="captcha"]');
-  if (await captchaFrame.locator('text=보안문자를 입력하세요').isVisible({ timeout: 3000 })) {
+  const captchaFrame = page.frameLocator('iframe[name^="c-"]'); // challenge iframe
+  const captchaTextarea = await captchaFrame.locator('#g-recaptcha-response').elementHandle();
+
+  if (captchaTextarea) {
     console.log('CAPTCHA 감지됨. 수동 입력 필요!');
     throw new Error('CAPTCHA가 감지되어 테스트를 중단합니다.');
   }
+
+
 }
