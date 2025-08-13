5b45204a6b5ddb1ec2502ddb4e0832911a92c214
Kang Hyeon Jun <rkd980609@naver.com>
2025-08-13 15:43:51 +0900

    remove

--- tests/mypage.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('마이페이지 접근', async ({ page }) => {
-  await page.goto('/');
-  await page.click('text=마이페이지');
-  await expect(page).toHaveURL(/mypage/);
-});
